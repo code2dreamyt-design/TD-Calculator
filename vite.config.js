3ff545b37985cda86bc2bbf15bb4de504e833c2f
code2dreamyt-design <code2dreamyt@gmail.com>
2026-04-26 16:27:05 +0530

    Refactor vite.config.js to use Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
-{
-  "buildCommand": "npm run build",
-  "outputDirectory": "dist",
-  "framework": "vite"
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),
+          tailwindcss(),
+  ],
+})
